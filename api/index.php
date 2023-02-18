5ac87c0996b9515d5c25490c7a0c65df398c4873
jeank93 <jcarlos1993@hotmail.com>
2023-02-18 12:46:34 -0600

    Add files via upload

---
+++ api/index.php
@@ -0,0 +1,41 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet"  crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js"  crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.min.js"  crossorigin="anonymous"></script>
+<link href="./content/css/controls.css" rel="stylesheet">
+<link href="./content/css/heroes.css" rel="stylesheet">
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta property="og:type" content="website">
+<meta property="og:title" content="Clearkey Hex to Base64 Converter">
+<meta property="og:description" content="Convertir Hex (CLK) en Base64 - Para Forocable">
+<title>Hex a Base64 (Forocable)</title>
+</head>
+<body>
+<div class="px-4 py-5 my-5 text-center"> <h1 class="display-5 fw-bold">CONVERTIR HEX EN BASE64 (FOROCABLE)</h1>  </br>
+   <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
+  
+			
+				  </div>
+	  </br>
+   <form class="p-4 p-md-5 border rounded-3 bg-light"id="signup-form" method="get" action="results2.php">
+         
+           <div class="form-floating mb-3">
+            <input type="keyid" class="form-control" name="keyid" id="keyid" placeholder="Key ID in Hex">
+            <label for="floatingPassword">Introducir el KID (HEX) Aqui!</label>
+          </div>
+		     <div class="form-floating mb-3">
+            <input type="key" class="form-control" name="key" id="key" placeholder="Key in Hex">
+            <label for="floatingPassword">Introducir el KEY (HEX) Aqui!</label>
+          </div>
+	
+          <button type="submit" class="w-100 btn btn-lg btn-primary"  id="btn-submit">GENERAR AHORA</button>
+          <hr class="my-4">
+          <small class="text-muted"><strong>Al hacer clic en Generar ahora, es posible que esté violando los términos de servicio de su plataforma de transmisión. No seremos responsables de ningún problema que produzca pérdidas o inconvenientes incurridos como resultado de dichos cambios o diferencias..</strong></small>
+        </form>
+	</body>
+	</html>
